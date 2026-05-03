b5db7f46b8e4b3ccb4923894697c27f4c550e995
Wervelin Silva <115909015+wervelin@users.noreply.github.com>
2026-05-03 18:45:48 -0300

    Refactor Home controller index method formatting

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,13 +1,12 @@
-<?php
-namespace App\Controllers;
+<?php namespace App\Controllers;
 
 class Home extends BaseController
 {
-	public function index()
-	{
-		return view('welcome_message');
-	}
+    public function index()
+    {
+        return view('welcome_message');
+    }
 
-	//--------------------------------------------------------------------
+    //--------------------------------------------------------------------
 
 }
