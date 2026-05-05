36af2a3801ba74ac0d45d051caa3ecd354a20037
Wervelin Silva <115909015+wervelin@users.noreply.github.com>
2026-05-05 15:07:37 -0300

    Delete ERP-Gestão-SaaS-V1 directory

--- ERP-Gestão-SaaS-V1/tests/_support/Language/en/Foo.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-   'bar'             => 'Foo Bar Translated',
-   'bar.min_length1' => 'The {field} field is very short.',
-   'bar.min_length2' => 'Supplied value ({value}) for {field} must have at least {param} characters.',
-];
